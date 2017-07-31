bdab07681b68e9ccad493a1d26e78b0cab67ff24
Igor V. Kruglenko <igor.kruglenko@gmail.com>
2017-07-31 12:41:41 +0300

    9

---
+++ 1/app/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+//= require jquery //= require turbolinks //= require materialize-sprockets //= require 
+//=jquery.turbolinks //= require materialize //= require turbolinks
+//= require jquery
+//= require jquery.turbolinks
+//= require materialize
+//= require turbolinks
+
+//= require app/assets/javascripts/materialize/extras
+//= materialize/extras/nouislider
